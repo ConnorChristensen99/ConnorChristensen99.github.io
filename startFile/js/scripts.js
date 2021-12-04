6ec45cee98f393147d9c9a059d28a32eae588884
Connor Christensen <connor.christensen@uvu.edu>
2021-12-03 18:59:24 -0700

    I believe I finished up 12

--- startFile/js/scripts.js
+++ startFile/js/scripts.js
@@ -1,5 +1,5 @@
 // what is the path to the JSON file?
-const apiURL = ""
+const apiURL = "hoteldata.json"
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
@@ -7,13 +7,41 @@ fetch(apiURL)
   .then((myList) => {
     //Once it comes back, display it to the console.
     console.log(myList);
+
+    for (i = 0; i < myList.length; i++) {
+    
+    let myImageTag = document.createElement("img");
+    myImageTag.src = myList[i].photo;
+    
+    let myCaptionTag = document.createElement("figcaption");
+    myCaptionTag.textContent = myList[i].name;
+
+    
+    let myFigureTag = document.createElement("figure");
+    myFigureTag.appendChild(myImageTag);
+    myFigureTag.appendChild(myCaptionTag);
+
+
+
+    let myStreetTag = document.createElement("p");
+    myStreetTag = textContent = myList[i].address;
+
+    let myPhoneTag = document.createElement("p");
+    myPhoneTag = textContent = myList[i].phone;
+   
+
+
+    let mySectionTag = document.createElement("section");
+    mySectionTag.appendChild(myFigureTag);
+    mySectionTag.appendChild(myPhoneTag);
+
+    
+
+      
+
     
     
     
-    
-    
-    
-    
-    
-    
+    document.getElementById('#').appendChild(mySectionTag);
+    }
 }); //end of "then" fat arrow function
